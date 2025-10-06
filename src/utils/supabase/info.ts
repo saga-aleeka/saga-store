e69ed0a2088ccc3d05e74d4f68452bc43114db6c
saga-aleeka <ariel.leeka@sagadiagnostics.com>
2025-10-06 20:22:47 +0000

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/utils/supabase/info.ts

--- src/utils/supabase/info.ts
+++ src/utils/supabase/info.ts
@@ -2,4 +2,4 @@
 
 // Correct Supabase config for frontend usage
 export const supabaseUrl = 'https://epsaukgvgadnaeplsgcr.supabase.co';
-export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwc2F1a2d2Z2FkbmFlcGxzZ2NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNzQxNDMsImV4cCI6MjA3NDg1MDE0M30.Md7ALQW2BJFwIJHL3ojg2TiBDM73F_Z98Bclp5KbHaM';
+export const supabaseAnonKey = 'sb_publishable_9MoTDYr319FY717-f3aldA_G7-b8WhR';
